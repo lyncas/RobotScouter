8999b85a8b05dd407240f633d29ea5f726d70fe5
Nolan Shah <nolanshah212@gmail.com>
2014-03-21 20:43:00 -0500

    Added Context Menu and Delete function to team list

--- RobotScouter/src/org/discobots/robotscouter/MainActivity.java
+++ RobotScouter/src/org/discobots/robotscouter/MainActivity.java
@@ -9,13 +9,10 @@ import org.discobots.robotscouter.data.Robot;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.support.v4.widget.SimpleCursorAdapter;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,30 +20,26 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	String[] contextMenuItems = { "Edit", "Delete" };
 	ArrayList<Robot> robots;
 	ArrayAdapter<Robot> adapter;
-	
-	public MainActivity() {
-		super();
-		this.robots = new ArrayList<Robot>();
-		System.out.println(robots.size());
-	}
-	
+	boolean deletionMode = false;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main); // Content UI stuff
 
-		
-		final MainActivity instance = this;
+		this.robots = new ArrayList<Robot>(); // Robot Data
+
+		final MainActivity instance = this; // UI List stuff
 		ListView listview = (ListView) findViewById(R.id.listView1);
 		listview.setVerticalScrollBarEnabled(true);
-		
+		this.registerForContextMenu(listview);
 		adapter = new ArrayAdapter<Robot>(this,
 				android.R.layout.simple_list_item_1, robots);
 		listview.setAdapter(adapter);
 		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
 			@Override
 			public void onItemClick(AdapterView<?> parent, final View view,
 					int position, long id) {
@@ -58,35 +51,62 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+	public boolean onCreateOptionsMenu(Menu menu) { // Action bar creator
 		getMenuInflater().inflate(R.menu.team_management, menu);
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void openPreScoutingActivity(Robot item) {;
+
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		if (v.getId() == R.id.listView1) {
+			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+			menu.setHeaderTitle("Options");
+			String[] menuItems = contextMenuItems;
+			for (int i = 0; i < menuItems.length; i++) {
+				menu.add(Menu.NONE, i, i, menuItems[i]);
+			}
+		}
+	}
+
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
+				.getMenuInfo();
+		int menuItemIndex = item.getItemId();
+		String[] menuItems = contextMenuItems;
+		String menuItemName = menuItems[menuItemIndex];
+		Robot r = this.robots.get(info.position);
+		if (menuItemName.equalsIgnoreCase(contextMenuItems[0])) {
+			openPreScoutingActivity(r);
+		} else if (menuItemName.equalsIgnoreCase(contextMenuItems[1])) {
+			this.adapter.remove(r);
+		} 
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) { // Action bar listener
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.add_team:
+			Robot r = new Robot();
+			this.adapter.add(r);
+			System.out.println(this.robots.size());
+			openPreScoutingActivity(r);
+			return true;
+		case R.id.action_settings:
+			// DO SOMETHING
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void openPreScoutingActivity(Robot item) {
 		Intent intent = new Intent(this, PreScoutingActivity.class);
 		intent.putExtra(PreScoutingActivity.ROBOT_DATA, item);
 		startActivity(intent);
 	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case R.id.add_team:
-	        	Robot r = new Robot();
-	        	this.adapter.add(r);
-	        	System.out.println(this.robots.size());
-	            openPreScoutingActivity(r);
-	            return true;
-	        case R.id.action_settings:
-	            // DO SOMETHING
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}
-	
+
 }
